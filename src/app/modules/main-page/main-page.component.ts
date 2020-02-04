fc507e5bce0b01087e95e0801acda00c89dc7694
weeehabla <weeehaplay@gmail.com>
2020-02-04 20:41:22 +0100

    setting up

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { ApolloService } from "../../core/apollo.service";
 @Component({
-  selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  selector: "app-main-page",
+  templateUrl: "./main-page.component.html",
+  styleUrls: ["./main-page.component.css"]
 })
 export class MainPageComponent implements OnInit {
-
-  constructor() { }
+  data: any = {};
+  arr: any = [];
+  public currentUser = localStorage.getItem("currentUser");
+  constructor(private apollo: ApolloService) {}
 
   ngOnInit() {
-  }
+    this.apollo.getUser(this.currentUser).subscribe(
+      res => {
+        console.log(res.data);
+        this.data = res.data.oneUser;
+        this.arr = this.data.userSubscription;
+      },
+      err => {
+        console.log(err);
+      }
+    );
 
+    //   this.apollo.getService().subscribe(
+    //     res => {
+    //       console.log(res.data);
+    //     },
+    //     err => {
+    //       console.log(err);
+    //     }
+    //   );
+    //   this.apollo.getSubscription().subscribe(
+    //     res => {
+    //       console.log(res.data);
+    //     },
+    //     err => {
+    //       console.log(err);
+    //     }
+    //   );
+  }
 }
