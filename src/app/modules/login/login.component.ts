142e20ef7c7adbf728d54bce632ffb1893949722
weeehabla <weeehaplay@gmail.com>
2020-02-05 11:57:15 +0100

    login  only user with subscription can login

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "../../core/auth.service";
 import { Router, ActivatedRoute } from "@angular/router";
+import { ApolloService } from "../../core/apollo.service";
 
 @Component({
   selector: "app-login",
@@ -12,12 +13,14 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   errors: any = [];
   notifyMessage = "";
+  public currentUser;
 
   constructor(
     private formbuilder: FormBuilder,
     private auth: AuthService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private apollo: ApolloService
   ) {}
 
   ngOnInit() {
@@ -64,12 +67,27 @@ export class LoginComponent implements OnInit {
           console.log(token.errors[0].message);
         } else {
           console.log("this is the user");
-          this.router.navigate(["/"]);
+          this.currentUser = localStorage.getItem("currentUser");
+          this.apollo.getUser(this.currentUser).subscribe(
+            res => {
+              //only user with the subscription can loged in so its even for the admin with subscription
+              // console.log(res.data.oneUser.userSubscription);
+
+              if (res.data.oneUser.userSubscription.length === 0) {
+                this.auth.logout();
+              } else {
+                console.log(res.data);
+                this.router.navigate(["/"]);
+              }
+            },
+            err => {
+              console.log(err);
+            }
+          );
         }
       },
       errorResponse => {
         console.log(errorResponse);
-        // this.errors = errorResponse.error.errors;
       }
     );
   }
