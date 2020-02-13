133a266501f2fcac36a3c6887fa61b6c864a4c03
weeehabla <weeehaplay@gmail.com>
2020-02-13 11:15:36 +0100

    pwa pushNotification

--- src/app/core/http.service.ts
+++ src/app/core/http.service.ts
@@ -4,8 +4,13 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class HttpService {
+  // public notificationUrl = "https://blocks-backend.herokuapp.com/push";
+  public notificationUrl = "http://localhost:4000/push";
   constructor(private http: HttpClient) {}
 
+  postSomething(sub: any) {
+    return this.http.post(this.notificationUrl, sub);
+  }
   gimmeJokes() {
     return this.http.get("https://api.chucknorris.io/jokes/random");
   }
--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ApolloService } from "../../core/apollo.service";
 import { AuthService } from "../../core/auth.service";
 import { HttpService } from "../../core/http.service";
+import { SwUpdate, SwPush } from "@angular/service-worker";
 @Component({
   selector: "app-main-page",
   templateUrl: "./main-page.component.html",
@@ -9,16 +10,21 @@ import { HttpService } from "../../core/http.service";
 })
 export class MainPageComponent implements OnInit {
   joke: any;
+  readonly VAPID_KEY =
+    "BIDKneMUisz3eBe-_YA5eA3qm_JAPv6Uz79IIWppgjakBOjpUQYK3E6BbBfcvQaGhKsnodIJ04VYrrvpv256erY";
   constructor(
     private apollo: ApolloService,
     private auth: AuthService,
-    private http: HttpService
+    private http: HttpService,
+    private SwUpdate: SwUpdate,
+    private SwPush: SwPush
   ) {}
 
   ngOnInit() {
     // this.http.gimmeJokes().subscribe(res => {
     //   this.joke = res;
     // });
+    this.subscribeToNotification();
     this.apollo.createBlock().subscribe(
       res => {
         console.log(res.data);
@@ -27,5 +33,17 @@ export class MainPageComponent implements OnInit {
         console.log(err);
       }
     );
+  }
+  subscribeToNotification() {
+    if (this.SwUpdate.isEnabled) {
+      this.SwPush.requestSubscription({
+        serverPublicKey: this.VAPID_KEY
+      }).then(sub => {
+        console.log(sub);
+        this.http.postSomething(sub).subscribe(res => {
+          console.log(res);
+        });
+      });
+    }
   }
 }
